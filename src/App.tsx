eda60d9874f747fcbf070dc592f7f090c2bd5365
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-18 20:41:05 -0300

    :lipstick: Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,100 @@
 import './styles/main.css';
 
+import logoImg from './assets/logo.svg';
+import gameImg01 from './assets/game_1.png';
+
 function App() {
   return (
-    <div className="text-cyan-500">
-      <h1>Hello World!</h1>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="logo" />
+
+      <h1 className="sm:text-6xl text-4xl text-white font-black mt-20">
+        Seu{' '}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{' '}
+        está aqui.
+      </h1>
+
+      <div className="grid xl:grid-cols-6 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-6 mt-16">
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src={gameImg01} />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src={gameImg01} />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src={gameImg01} />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src={gameImg01} />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src={gameImg01} />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src={gameImg01} />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="xl:w-full rounded-lg pt-1 bg-nlw-gradient self-center overflow-hidden mt-8 mx-3">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center flex-wrap gap-6">
+          <div>
+            <strong className="block font-bold text-2xl text-white">
+              Não encontrou seu duo?
+            </strong>
+            <span className="block text-zinc-400">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded">
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ["./src/**/*.tsx", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background.png')",
+        'nlw-gradient': 'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      },
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'xs': '480px',
+    },
   },
   plugins: [],
 }
