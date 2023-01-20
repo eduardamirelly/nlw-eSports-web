7407d7e8bc8ecf08d493902ece72f20b50376840
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-20 19:46:57 -0300

    :art: add CreateAdBanner component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/main.css';
 import logoImg from './assets/logo.svg';
 import gameImg01 from './assets/game_1.png';
 import { MagnifyingGlassPlus } from 'phosphor-react';
+import { CreateAdBanner } from './components/CreateAdBanner';
 
 function App() {
   return (
@@ -17,86 +18,9 @@ function App() {
         está aqui.
       </h1>
 
-      <div className="grid xl:grid-cols-6 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-6 mt-16">
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src={gameImg01} />
+      <div className="grid xl:grid-cols-6 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-6 mt-16"></div>
 
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              League of Legends
-            </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src={gameImg01} />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              League of Legends
-            </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src={gameImg01} />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              League of Legends
-            </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src={gameImg01} />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              League of Legends
-            </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src={gameImg01} />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              League of Legends
-            </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-        <a href="#" className="relative rounded-lg overflow-hidden">
-          <img src={gameImg01} />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">
-              League of Legends
-            </strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>
-        </a>
-      </div>
-
-      <div className="xl:w-full rounded-lg pt-1 bg-nlw-gradient self-center overflow-hidden mt-8 mx-3">
-        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center flex-wrap gap-6">
-          <div>
-            <strong className="block font-bold text-2xl text-white">
-              Não encontrou seu duo?
-            </strong>
-            <span className="block text-zinc-400">
-              Publique um anúncio para encontrar novos players!
-            </span>
-          </div>
-
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center justify-center gap-3">
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
+      <CreateAdBanner />
     </div>
   );
 }
---
+++ src/components/CreateAdBanner.tsx
@@ -0,0 +1,23 @@
+import { MagnifyingGlassPlus } from 'phosphor-react';
+
+export function CreateAdBanner() {
+  return (
+    <div className="xl:w-full rounded-lg pt-1 bg-nlw-gradient self-center overflow-hidden mt-8 mx-3">
+      <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center flex-wrap gap-6">
+        <div>
+          <strong className="block font-bold text-2xl text-white">
+            Não encontrou seu duo?
+          </strong>
+          <span className="block text-zinc-400">
+            Publique um anúncio para encontrar novos players!
+          </span>
+        </div>
+
+        <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center justify-center gap-3">
+          <MagnifyingGlassPlus size={24} />
+          Publicar anúncio
+        </button>
+      </div>
+    </div>
+  );
+}
